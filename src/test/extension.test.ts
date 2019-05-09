c83e705ecbd69d51b58270b51750611dacffa009
Johny.li <johny.li@selmit.com>
2019-05-09 17:51:23 +0800

    1. bump version 2. replace mocha with jest

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -12,7 +12,7 @@ import * as vscode from 'vscode';
 import * as myExtension from '../extension';
 
 // Defines a Mocha test suite to group tests of similar kind together
-suite("Extension Tests", () => {
+describe("Extension Tests", () => {
 
     // Defines a Mocha unit test
     test("Something 1", () => {
